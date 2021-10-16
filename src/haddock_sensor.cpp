57724eba29f3a9c8905f4377d73c76624101b44a
Matti Lattu <matti.lattu@iki.fi>
2021-10-16 09:11:24 +0300

    Send sensor metadata

--- src/haddock_sensor.cpp
+++ src/haddock_sensor.cpp
@@ -35,26 +35,38 @@ ReactESP app([]() {
   const char* sk_path = "propulsion.port.revolutions";
   const char* analog_in_config_path = "/random_sensor";
 
+  SKMetadata* metadata = new SKMetadata();
+  metadata->units_ = "Hz";
+  metadata->description_ = "Propeller Revolutions Port Side";
+  metadata->display_name_ = "Propeller Port Side";
+  metadata->short_name_ = "Propeller Port";
+
   uint min_limit = 0;
   uint max_limit = 4000;
   uint read_delay = 500;
 
   auto* random_sensor = new RandomSensor(min_limit, max_limit, read_delay, analog_in_config_path);
 
-  random_sensor->connect_to(new SKOutputNumber(sk_path));
+  random_sensor->connect_to(new SKOutputNumber(sk_path, analog_in_config_path, metadata));
 #endif
 
 #ifdef SENSOR_RANDOM_ENGINE_STB
   const char* sk_path = "propulsion.stb.revolutions";
   const char* analog_in_config_path = "/random_sensor";
 
+  SKMetadata* metadata = new SKMetadata();
+  metadata->units_ = "Hz";
+  metadata->description_ = "Propeller Revolutions Starboard Side";
+  metadata->display_name_ = "Propeller Stb Side";
+  metadata->short_name_ = "Propeller Stb";
+
   uint min_limit = 0;
   uint max_limit = 4000;
   uint read_delay = 500;
 
   auto* random_sensor = new RandomSensor(min_limit, max_limit, read_delay, analog_in_config_path);
 
-  random_sensor->connect_to(new SKOutputNumber(sk_path));
+  random_sensor->connect_to(new SKOutputNumber(sk_path, analog_in_config_path, metadata));
 #endif
 
   // Start the SensESP application running

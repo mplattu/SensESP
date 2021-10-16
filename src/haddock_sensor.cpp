28dae9bcb71c71e222384551ef55daefaa5849ca
Matti Lattu <matti.lattu@iki.fi>
2021-10-16 08:24:00 +0300

    Unified all sensor code to one file

--- src/haddock_sensor.cpp
+++ src/haddock_sensor.cpp
@@ -1,14 +1,15 @@
 #ifndef SENSOR_MAIN
 #define SENSOR_MAIN
 
+#ifdef SENSOR_TYPE_RANDOM
 #include "sensors/random_sensor.h"
+#endif
 
 #include <Arduino.h>
 
 #include "sensesp_app.h"
 #include "sensesp_app_builder.h"
 #include "signalk/signalk_output.h"
-#include "transforms/linear.h"
 
 // SensESP builds upon the ReactESP framework. Every ReactESP application
 // defines an "app" object vs defining a "main()" method.
@@ -23,28 +24,15 @@ ReactESP app([]() {
   SensESPAppBuilder builder;
 
   sensesp_app = builder.set_standard_sensors(UPTIME)
-    ->set_hostname("RandomSensorEnginePort")
+    ->set_hostname(HOSTNAME)
     ->set_sk_server(SERVER_ADDRESS, SERVER_PORT) // Don't specify server address or port
     ->set_wifi(WIFI_NAME, WIFI_PASS)
     //->set_led_pin(13);
 //    ->set_led_blinker(true, 1000, 2500, 4000);
     ->get_app();
 
-  // The "Signal K path" identifies this sensor to the Signal K server. Leaving
-  // this blank would indicate this particular sensor (or transform) does not
-  // broadcast Signal K data.
-  // To find valid Signal K Paths that fits your need you look at this link:
-  // https://signalk.org/specification/1.4.0/doc/vesselsBranch.html
+#ifdef SENSOR_RANDOM_ENGINE_PORT
   const char* sk_path = "propulsion.port.revolutions";
-
-  // The "Configuration path" is combined with "/config" to formulate a URL
-  // used by the RESTful API for retrieving or setting configuration data.
-  // It is ALSO used to specify a path to the SPIFFS file system
-  // where configuration data is saved on the MCU board. It should
-  // ALWAYS start with a forward slash if specified. If left blank,
-  // that indicates this sensor or transform does not have any
-  // configuration to save, or that you're not interested in doing
-  // run-time configuration.
   const char* analog_in_config_path = "/random_sensor";
 
   uint min_limit = 0;
@@ -54,6 +42,20 @@ ReactESP app([]() {
   auto* random_sensor = new RandomSensor(min_limit, max_limit, read_delay, analog_in_config_path);
 
   random_sensor->connect_to(new SKOutputNumber(sk_path));
+#endif
+
+#ifdef SENSOR_RANDOM_ENGINE_STB
+  const char* sk_path = "propulsion.stb.revolutions";
+  const char* analog_in_config_path = "/random_sensor";
+
+  uint min_limit = 0;
+  uint max_limit = 4000;
+  uint read_delay = 500;
+
+  auto* random_sensor = new RandomSensor(min_limit, max_limit, read_delay, analog_in_config_path);
+
+  random_sensor->connect_to(new SKOutputNumber(sk_path));
+#endif
 
   // Start the SensESP application running
   sensesp_app->enable();
